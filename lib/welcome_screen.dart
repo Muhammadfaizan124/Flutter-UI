93ea56746193092f418865f21f786ba122b80586
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-04-08 00:50:15 +0500

    few changes

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -1,18 +1,19 @@
 //import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:google/login_screen.dart';
 
-class welcomeScreen extends StatelessWidget {
-  const welcomeScreen({super.key});
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key); // corrected constructor
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.greenAccent,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               "PALESTINE",
               style: TextStyle(
                 color: Colors.white,
@@ -20,12 +21,21 @@ class welcomeScreen extends StatelessWidget {
                 height: 0.8,
               ),
             ),
-            Text(
+            const Text(
               "FREE PALESTINE FREE GAZA",
               style: TextStyle(
                 fontSize: 15,
                 color: Colors.white,
               ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                );
+              },
+              child: Text("lets go"),
             ),
           ],
         ),
